b987e24db86760d262f2bd649c63ec2ab513aee5
monicandani <33404531+monicandani@users.noreply.github.com>
2018-08-15 15:28:07 +0700

    keluhann

--- application/views/form_data_keluhan.php
+++ application/views/form_data_keluhan.php
@@ -30,21 +30,6 @@
 
                                         ?>
                                         <tr>
-<<<<<<< HEAD
-                                            <td>1</td>
-                                            <td> <a class="" href="<?php echo base_url();?>c_main/area">Jatinegara</a></td>
-                                            <td><a class="" href="" data-toggle="modal" data-target="#myModal"></a></td>
-                                            <td>Penyebab</td>
-                                            <td>Solusi</td>
-                                            <td> 
-                <a class="btn btn-primary" href="<?php echo base_url();?>c_main/waktu">Lihat</button></a></td>
-                                            <td><center>
-                <button class="btn btn-default" href="<?php echo base_url();?>c_form/edit_data_keluhan">Edit</button></center>
-                </td>
-                <td><center>
-        <button class="btn btn-danger" onclick='delete_user(<?php echo $u->id ?>)' data-toggle="modal" data-target="#myModal">Hapus</button></center>
-            </td>
-=======
                                             <td><?php echo $no++ ?></td>
                                             <td>Area</td>
                                             <td>jenis keluhan</td>
@@ -53,12 +38,11 @@
                                             <td><a class="btn btn-primary" href="<?php echo base_url();?>c_main/waktu">Lihat</button></a></td>
                                             <td>
 
-                                                 <form method='' action="<?php echo base_url('c_layanan/edit_layanan/'.$l->sid) ?>">
+                                                 <form method='' action="<?php //echo base_url('c_layanan/edit_layanan/'.$l->sid) ?>">
                                                     <button class='btn btn-default' type='submit'>Edit</button>
                                                 </form>
-                                                <button data-toggle="modal" data-target="#exampleModal" onclick="set_id(<?php echo $l->sid ?>)" class="btn btn-danger">Hapus</button>
+                                                <button data-toggle="modal" data-target="#exampleModal" onclick="set_id(<?php// echo $l->sid ?>)" class="btn btn-danger">Hapus</button>
                                             </td>
->>>>>>> e578b1bb4025dbf9b6a2c3922eadf63b0e7d186c
                                         </tr>
                                         <?php } ?>
                                        
