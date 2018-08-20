57789faefeb8bb1f6d0a81a88f4163f850178653
tiwnab <33388986+tiwnab@users.noreply.github.com>
2018-08-20 13:14:48 +0700

    yth

--- application/views/form_jenis_gangguan.php
+++ application/views/form_jenis_gangguan.php
@@ -35,7 +35,7 @@
                             </div>
                         </div>
                     </div>
-                     <!-- Table -->
+                     <!-- Table  -->
                 </div>
             </div>
         </div>
