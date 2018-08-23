71e84f9a7bab55fc4d79df2d83a983542e763158
tiwnab <33388986+tiwnab@users.noreply.github.com>
2018-08-23 08:16:40 +0700

    nnn

--- application/views/form_layanan.php
+++ application/views/form_layanan.php
@@ -63,7 +63,7 @@
             </div>
         </div>
     </div>
-
+<!---------dd-->
     <!-- Modal Hapus -->
               <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                   <div class="modal-dialog" role="document">
